9133c871e51096e251b25977d6d3733ce8beb830
dhiren-rajesh <81640330+dhiren-rajesh@users.noreply.github.com>
2021-04-27 12:31:53 +0530

    Twitter Side Bar Files
    Added the react.js code for the side bar in my Twitter Clone

---
+++ App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Sidebar from './Sidebar'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Twitter Clone</h1>
+
+      {/* Sidebar */}
+        <Sidebar/>
+      {/* Feed */}
+
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
